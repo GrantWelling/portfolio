f8883a90c49bc401e3a33fa7de85ff2d3c4868ca
GrantWelling <115586241+GrantWelling@users.noreply.github.com>
2024-05-31 16:32:36 -0400

    Preliminary Setup

--- index.php
+++ index.php
@@ -1,31 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Grant Welling</title>
+<title>My Portfolio</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/mediaqueries.css">
 
 </head>
 <body>
-    <ul>
-        <li><a>Contact</a></li>
-        <li><a>About</a></li>
-        <li><a>Projects</a></li>
-    </ul>
-
-    <div class="header">
-        <div class="headshot">
-            <img src="images/headshot2.jpg">
-        </div>
-        <div class="nameTitle">
-            <h1>Grant Welling<br>
-                Software Engineer</h1>
-        </div>
-    </div>
-
-
-    
+    <nav id="desktop.nav">
+        <div class="logo">Grant Welling</div>
+        <div>
+            <ul class="nav-links">
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+    <script src="script.php"></script>
 </body>
 </html>
