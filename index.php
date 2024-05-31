e01c3fa04916bb20200f8706b60f3d47df548cb5
GrantWelling <115586241+GrantWelling@users.noreply.github.com>
2024-05-31 16:04:10 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Grant Welling</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <ul>
+        <li><a>Contact</a></li>
+        <li><a>About</a></li>
+        <li><a>Projects</a></li>
+    </ul>
+
+    <div class="header">
+        <div class="headshot">
+            <img src="images/headshot2.jpg">
+        </div>
+        <div class="nameTitle">
+            <h1>Grant Welling<br>
+                Software Engineer</h1>
+        </div>
+    </div>
+
+
+    
+</body>
+</html>
